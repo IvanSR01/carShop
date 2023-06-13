8ef6680509ba11577d445e716e06911e00a4f250
zxcivan07 <108341880+zxcivan07@users.noreply.github.com>
2023-06-13 12:32:56 +0300

    Update router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,5 @@
 import { FC } from 'react'
-import Home from '../pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
-import Store from '../pages/Store/Store'
-import Basket from '../pages/Basket/Basket'
-import Favorite from '../pages/Favorite/Favorite'
 import { data } from './RouterData'
 const Router: FC = () => {
 	return (
